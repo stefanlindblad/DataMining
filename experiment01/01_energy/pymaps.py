f43772e5333bebcac8c5ef7f95f9ec74709265d0
Daniel Grießhaber <dangrie158@gmail.com>
2014-04-24 18:03:33 +0200

    exercise 2.4

--- experiment01/01_energy/pymaps.py
+++ experiment01/01_energy/pymaps.py
@@ -241,24 +241,3 @@ class PyMap:
 </html>
 """ % (self.pymapjs())
         return self.html
-
-
-if __name__ == "__main__":
-    import sys
-    
-    g = PyMap()                         # creates an icon & map by default
-    icon2 = Icon('icon2')               # create an additional icon
-    icon2.image = "http://labs.google.com/ridefinder/images/mm_20_blue.png" # for testing only!
-    icon2.shadow = "http://labs.google.com/ridefinder/images/mm_20_shadow.png" # do not hotlink from your web page!
-    g.addicon(icon2)
-    g.key = "ABQIAAAAQQRAsOk3uqvy3Hwwo4CclBTrVPfEE8Ms0qPwyRfPn-DOTlpaLBTvTHRCdf2V6KbzW7PZFYLT8wFD0A" # you will get your own key
-    g.maps[0].zoom = 10
-    q = [48,9]                           # create a marker with the defaults
-    r = [2,2,'','icon2']                # icon2.id, specify the icon but no text
-    s = [3,3,'hello, <u>world</u>']     # don't specify an icon & get the default
-    g.maps[0].setpoint(q)               # add the points to the map
-    g.maps[0].setpoint(r)
-    g.maps[0].setpoint(s)
-    
-##    print g.showhtml()
-    open('test.htm','wb').write(g.showhtml())   # generate test file
