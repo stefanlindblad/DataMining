05541325733927201b8859f8eeea9c118c25ea01
Daniel Grießhaber <dangrie158@gmail.com>
2014-04-24 12:16:10 +0200

    exercise 2.2.1.4

--- experiment01/01_energy/energyClustering.py
+++ experiment01/01_energy/energyClustering.py
@@ -5,6 +5,8 @@ from sklearn import preprocessing
 from numpy import *
 import matplotlib.pyplot as plt
 
+NUM_CLUSTER = 4
+
 energyInfo = pandas.read_csv('../resources/EnergyMix.csv')
 reducedEnergyInfo = energyInfo.drop("Country", 1)
 reducedEnergyInfo = reducedEnergyInfo.drop("Total2009", 1)
@@ -17,13 +19,31 @@ relativeEnergyConsumption = spatial.distance.pdist(preprocessedEnergyInfo, metri
 linkageMatrix = cluster.hierarchy.linkage(relativeEnergyConsumption)
 cluster.hierarchy.dendrogram(linkageMatrix, orientation = 'left', labels = energyInfo.Country.values)
 
-clustlabels = cluster.hierarchy.fcluster(linkageMatrix, 4, criterion='maxclust')
+clustlabels = cluster.hierarchy.fcluster(linkageMatrix, NUM_CLUSTER, criterion='maxclust')
 clustlabels -= 1
 
-for cl in range(4):
+plt.figure(1)
+for cl in range(NUM_CLUSTER):
     print '-'*10 + 'Cluster ' + str(cl) + '-'*10
     ind=find(clustlabels==cl)
     for a in ind:
         print energyInfo.Country.values[a]
 
+plt.figure(2)
+
+
+for index, cl in enumerate(clustlabels):
+    plt.subplot(NUM_CLUSTER, 1, cl)
+    plt.plot(reducedEnergyInfo.values[index,:])
+
+for cl in range(NUM_CLUSTER):
+    plt.subplot(NUM_CLUSTER, 1, cl)
+    plt.title('Cluster ' + str(cl))
+    plt.xticks(range(5), reducedEnergyInfo.columns.values)
+
+
+
 plt.show()
+
+
+
