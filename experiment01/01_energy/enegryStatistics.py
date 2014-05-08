97ee57bcb25e22efc85944ab5199b32933a372fb
Oliver Fesseler <ofesseler@gmail.com>
2014-05-08 13:26:13 +0200

    fixed energyStatistics.py

--- experiment01/01_energy/enegryStatistics.py
+++ experiment01/01_energy/enegryStatistics.py
@@ -1,5 +1,8 @@
+
 import pandas
 import matplotlib.pyplot as plt
+
+
 
 ENERGYFORMS = ['Oil', 'Gas', 'Coal', 'Nuclear', 'Hydro']
 NUM_ENERGYFORMS = len(ENERGYFORMS)
@@ -7,8 +10,19 @@ NUM_ENERGYFORMS = len(ENERGYFORMS)
 energyInfo = pandas.read_csv('../resources/EnergyMixGeo.csv')
 print energyInfo.describe()
 
+energyFormList = list(energyInfo.columns.values)[1:-2]
 reducedEnergyInfo = energyInfo[ENERGYFORMS]
 
-plt.figure(1)
+plt.figure("Energyconsumption by energyform in seperate subboxplots")
+i = 0
+for energyForm in reducedEnergyInfo:
+	i += 1
+	plt.subplot(1, 5, i)
+	plt.xlabel(energyForm)
+	plt.boxplot(energyInfo[energyForm].values, sym='')
+	plt.grid(True)
+
+plt.figure("Energyconsumption Energyconsumption by energyform in one plot")
 plt.boxplot(reducedEnergyInfo.values, sym='')
+plt.xticks(range(1, NUM_ENERGYFORMS+1), ENERGYFORMS)
 plt.show()
