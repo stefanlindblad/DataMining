9756511eb917c0f4f668d0fa87d1827aab7fb398
Maria Floruss <maria@floruss.net>
2014-06-05 13:20:10 +0100

    added functions incf(), incc(), fcount(), catcount() and totalcount() plus unittests.

--- experiment03/scripts/docclass.py
+++ experiment03/scripts/docclass.py
@@ -1,4 +1,4 @@
-def getwords(self, doc, minWordLength = 3, maxWordLength = 20):
+def getwords(doc, minWordLength = 3, maxWordLength = 20):
     splittedStringList = doc.lower().split(" ")
     dict = {}
     for token in splittedStringList:
@@ -8,11 +8,33 @@ def getwords(self, doc, minWordLength = 3, maxWordLength = 20):
     return dict
 
 class Classifier():
-    getfeatures = getwords
-    
-    def __init__(self):
+
+    def __init__(self, featureFunction):
         self.fc = {}
         self.cc = {"Bad" : 0, "Good" : 0}
+        self.getfeatures = featureFunction
+
+    def incf(self, f, cat):
+        if(not self.fc.has_key(f)):
+            self.fc[f] = {"Good":0, "Bad":0}
+        self.fc[f][cat] += 1
+
+    def incc(self,cat):
+        self.cc[cat] += 1
+
+    def fcount(self,f,cat):
+        if(self.fc.has_key(f)):
+            return self.fc[f][cat]
+        else:
+            return 0
+
+    def catcount(self,cat):
+        return self.cc[cat]
+
+    def totalcount(self):
+        return self.cc["Good"] + self.cc["Bad"]
+
+
 
 
 
--- experiment03/scripts/test_getwords.py
+++ experiment03/scripts/test_getwords.py
@@ -1,16 +1,59 @@
 from unittest import TestCase
 from docclass import Classifier
+from docclass import getwords
 
 __author__ = 'maria'
 
 
 class TestGetwords(TestCase):
 
-    def testClassifier(self):
-        c = Classifier()
+    def testGetFeatures(self):
+        c = Classifier(getwords)
         dict = c.getfeatures("Hello World world world ,       hello has cats and vervyveryveryveryveryverylongword")
         self.assertIsNotNone(dict)
         self.assertIsNotNone(dict["hello"])
         self.assertEqual(dict["hello"], 1)
         self.assertFalse(dict.has_key("has"))
         self.assertFalse(dict.has_key("vervyveryveryveryveryverylongword"))
+
+    def testIncF(self):
+        c = Classifier(getwords)
+        c.incf("hello", "Good")
+        self.assertEqual(c.fc["hello"]["Good"], 1)
+        c.incf("hello", "Good")
+        self.assertEqual(c.fc["hello"]["Good"], 2)
+        c.incf("hello", "Bad")
+        self.assertEqual(c.fc["hello"]["Bad"], 1)
+
+    def testIncC(self):
+        c = Classifier(getwords)
+        c.incc("Bad")
+        self.assertEqual(c.cc["Bad"], 1)
+        c.incc("Bad")
+        self.assertEqual(c.cc["Bad"], 2)
+        c.incc("Good")
+        self.assertEqual(c.cc["Good"], 1)
+
+    def testFCount(self):
+        c = Classifier(getwords)
+        c.incf("hello", "Good")
+        c.incf("hello", "Good")
+        c.incf("hello", "Bad")
+        self.assertEqual(c.fcount("hello", "Good"), 2)
+        self.assertEqual(c.fcount("hello", "Bad"), 1)
+        self.assertEqual(c.fcount("wurst", "Bad"), 0)
+
+    def testCatCount(self):
+        c = Classifier(getwords)
+        c.incc("Bad")
+        c.incc("Bad")
+        c.incc("Good")
+        self.assertEqual(c.catcount("Good"), 1)
+        self.assertEqual(c.catcount("Bad"), 2)
+
+    def testTotalCount(self):
+        c = Classifier(getwords)
+        c.incc("Bad")
+        c.incc("Bad")
+        c.incc("Good")
+        self.assertEqual(c.totalcount(), 3)
